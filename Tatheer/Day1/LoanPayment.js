8a5cfc6ffcc963134aabcd10095ba95ed9b9c706
Erum-Abdul-Latif <87967100+Erum-Abdul-Latif@users.noreply.github.com>
2022-06-23 17:53:52 +0500

    Delete Tatheer directory

--- Tatheer/Day1/LoanPayment.js
+++
@@ -1,6 +0,0 @@
-var a;
-var p= prompt('Enter your loan amount : ');
-var r = prompt('Enter nterest rate per period : ');
-var n = prompt('Enter total number of periods :  ');
-a=p*(r*(1+r)^n) / ( (1+r)^n -1 );
-alert("Payment amount per period : " + a);
